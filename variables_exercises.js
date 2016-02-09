f3baff2194838157c6e61597a3aad907b890870f
tanner466 <tanner466@gmail.com>
2016-02-09 14:46:20 -0500

    Complete variables exercises.

---
+++ variables_exercises.js
@@ -0,0 +1,9 @@
+var numChildren = 2;
+var partnerName = "Lyna";
+var geoLocation = "Toronto";
+var jobTitle = "Software Stud";
+
+var fortune = "You will be a " + jobTitle + " in " + geoLocation
+  + ", and married to " + partnerName + " with " + numChildren + " kids.";
+
+console.log(fortune);
